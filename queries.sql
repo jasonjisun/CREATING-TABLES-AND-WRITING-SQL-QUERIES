294b10b72c75fd792b22f80b72df2e742e563a9f
jasonjisun <159624189+jasonjisun@users.noreply.github.com>
2024-09-03 10:00:28 +0800

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,30 @@
+SELECT * 
+FROM Users 
+WHERE DateOfBirth BETWEEN '2000-01-01' AND '2004-12-31';
+
+SELECT * 
+FROM Posts 
+WHERE PostedBy = 4;
+
+SELECT GroupName 
+FROM Groups;
+
+SELECT * 
+FROM GroupMembershipRequests 
+WHERE GroupMemberUserID = 2;
+
+SELECT * 
+FROM Friends 
+WHERE FriendWhoAdded = 2 OR FriendBeingAdded = 2;
+
+SELECT * 
+FROM Friends 
+WHERE FriendWhoAdded = 1;
+
+SELECT * 
+FROM Posts 
+WHERE GroupID = 2 AND IsOnlyForFriends = FALSE;
+
+SELECT * 
+FROM GroupMembershipRequests 
+WHERE GroupID = 2 AND IsGroupMemberShipAccepted = FALSE;
